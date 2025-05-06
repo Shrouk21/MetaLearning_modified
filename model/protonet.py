682d9831d03dcd2157e55589a1eb4f8471e3a2f3
Shrouk21 <158491651+Shrouk21@users.noreply.github.com>
2025-05-06 18:30:15 +0300

    solve issues

--- model/protonet.py
+++ model/protonet.py
@@ -1,7 +1,9 @@
-import torch.nn as nn
+import torch
+import torch.nn as nn  # Import nn for layers
+import torch.nn.init as init  # Import init for weight initialization
 import math
 
-#Build conv layer for feature extraction
+# Build conv layer for feature extraction
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, retain_activation=True):
         super().__init__()
@@ -18,9 +20,10 @@ class ConvBlock(nn.Module):
         return self.block(x)
 
 
-#Build the embedding network (mapping feature space to fixed vector)
+# Build the embedding network (mapping feature space to fixed vector)
 class ProtoNetEmbedding(nn.Module):
     def __init__(self, x_dim=3, h_dim=64, z_dim=64, retain_last_activation=True):
+        super().__init__()
         self.encoder = nn.Sequential(
             ConvBlock(x_dim, h_dim),
             ConvBlock(h_dim, h_dim),
@@ -28,16 +31,16 @@ class ProtoNetEmbedding(nn.Module):
             ConvBlock(h_dim, z_dim, retain_activation=retain_last_activation)
         )
 
-        self.__init__weights() #Explicitly initialize the weights
+        self.__init__weights()  # Explicitly initialize the weights
 
     def __init__weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                init.kaiming_normal(m.weight, nonlinearity='relu')
+                init.kaiming_normal_(m.weight, nonlinearity='relu') 
             elif isinstance(m, nn.BatchNorm2d):
-                init.constant(m.weight, 1)
-                init.constant(m.bias, 0)
+                init.constant_(m.weight, 1) 
+                init.constant_(m.bias, 0)
+
     def forward(self, x):
-        x= self.encoder(x)
+        x = self.encoder(x)
         return x.view(x.size(0), -1)
-                
