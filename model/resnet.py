399a23c593bd6ac2e23b5457e30b07c94b76d1b9
Shrouk21 <158491651+Shrouk21@users.noreply.github.com>
2025-05-12 06:58:47 +0300

    fix issues

--- model/resnet.py
+++ model/resnet.py
@@ -8,8 +8,15 @@ from model.dropblock import DropBlock
 class convblock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
         super().__init__()
-        return nn.Conv2d(in_channels, out_channels, 3, padding=1, stride=stride, bias=False)
-    
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
 
 
 class BasicBlock(nn.Module):
